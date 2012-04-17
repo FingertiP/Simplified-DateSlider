8835d5614c3f030e18569f7e082ae14093510aa1
Warut Surapat <s.warut@gmail.com>
2012-04-18 04:04:05 +0700

    add installation script to make your life easier

---
+++ install.rb
@@ -0,0 +1,74 @@
+#puts File.dirname(__FILE__)
+require 'fileutils'
+if ARGV[0]== "--help" || ARGV[0] == "-h"
+
+elsif ARGV.length == 2
+  package_name     = ARGV[0]
+  project_location = ARGV[1]
+
+  r_required_files = %w[DateSlider.java DefaultDateSlider.java ScrollLayout.java]
+
+  #copy files src to target's src folder
+  FileUtils.cp_r "src", "#{project_location}"
+  date_slider_dir        = Dir.open("src/th/in/fingertip/dateslider")
+  target_date_slider_dir = Dir.open("#{project_location}/#{date_slider_dir.path}")
+
+  r_required_files.each do |file_name|
+    file = File.open("#{target_date_slider_dir.path}/#{file_name}", "r+")
+
+    #modify code
+    codes = file.readlines
+    codes.insert(1, "import #{package_name}.R;\n");
+
+    #write back
+    file.rewind
+    file.write(codes.join)
+    file.close
+  end
+
+  #copy res/drawable
+  FileUtils.cp_r "res/drawable", "#{project_location}/res/"
+
+  #copy layout
+  FileUtils.cp_r "res/layout", "#{project_location}/res/"
+
+  #copy attrs values
+  if(File.exists? "#{project_location}/res/values/attrs.xml")
+    original_file = File.open("res/values/attrs.xml", "r")
+    chunks        = original_file.readlines[2..7]
+    target_file   = File.open("#{project_location}/res/values/attrs.xml", "r+")
+    codes         = target_file.readlines
+    codes.insert(-2, chunks.join)
+    target_file.rewind
+    target_file.write(codes.join)
+    original_file.close
+    target_file.close
+  else
+    FileUtils.cp_r "res/values/attrs.xml", "#{project_location}/res/values"
+  end
+
+  #copy styles values
+  if(File.exists? "#{project_location}/res/values/styles.xml")
+    original_file = File.open("res/values/styles.xml", "r")
+    chunks        = original_file.readlines[2..7]
+    target_file   = File.open("#{project_location}/res/values/styles.xml", "r+")
+    codes         = target_file.readlines
+    codes.insert(-2, chunks.join)
+    target_file.rewind
+    target_file.write(codes.join)
+    original_file.close
+    target_file.close
+  else
+    FileUtils.cp "res/values/styles.xml", "#{project_location}/res/values/"
+  end
+
+  puts "Completed."
+else
+  puts <<-END
+    usage:    ruby install.rb [package_name] [project_location]
+              [package_name] a name of your default package in your project
+              [project_location] a path to your project
+
+    example:  ruby install.rb com.example ~/Projects/eclipse/workspace/myproject
+  END
+end
